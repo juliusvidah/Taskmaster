90e405c523b5d0e7e00197d81f5bf99c233f1bea
stephrett <stephenpanret1@gmail.com>
2024-11-30 11:44:00 +0100

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const userController = require('../controllers/user.controller');
 const router = Router();
 
-router.post('/', userController.create); // Route to create a new user
+router.post('/signup', userController.create); // Route to create a new user
 router.post('/login', userController.login); // Route for user login
 router.get('/', userController.getAll); // Route to get all users
 
